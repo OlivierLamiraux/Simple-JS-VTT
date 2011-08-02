f3a366fd96c243b03526d741ad5f448295f3e8ed
Olivier Leap <leap@spiderman.zzn.com>
2011-08-02 15:40:04 +0200

    Parse Script with JSHint

--- js/battlefield.js
+++ js/battlefield.js
@@ -1,104 +1,107 @@
+/*jshint onevar: true, strict: true, curly: true, eqeqeq: true, laxbreak: true */
 ///////////////////////////////////////
 //// BattleField
 function BattleField(w, h)
 {
-	var isEditor = false;
-	var editorColor = "#FFFFFF";
-	var isEditorMode = false;
-	var table = $("<table class='field'></table>");
-	var width = w;
-	var height = h;
-	var penPix = 1;
-	this.Constructor = function(w, h) {
-		var tr = "<tr/>";
-		var td = "<td/>";
-		var cell = "<div class='cell'></div>";
-		var cellPix = "<div class='cellPix'></div>";
-		
-		for(i=0; i < h; i++)
-		{
-			table.append(tr);
-		}
-		for(i=0; i < w; i++)
-		{
-			table.find('tr').append(td);
-		}
-		
-		table.find('tr td').each(function(index) {
-			var fieldCell = $(cell);
-			for(i=0; i < 10; i++)
-			{
-				for(j=0; j < 10; j++)
-				{
-					var c = $(cellPix);
-					var x = (index%width) * 10 + j;
-					var y = Math.floor(index/width) * 10 + i;
-					c.data("x", x);
-					c.data("y", y);
-					c.attr('id', 'cell_' + x + '_' + y);
-					c.css("background-color", "green");;
-					fieldCell.append(c);
-				}
+    "use strict";
+    var isEditor = false,
+        editorColor = "#FFFFFF",
+        isEditorMode = false,
+        table = $("<table class='field'></table>"),
+        width = w,
+        height = h,
+        penPix = 1,
+    
+        OnClick = function () {
+            if (isEditor) {
+                isEditorMode = !isEditorMode;
+            }
+        
+            if (isEditorMode) {
+                table.find("td").css("border", "1px solid red");
+            }
+            else {
+                table.find("td").css("border", "1px solid black");
+            }
+        },
+    
+        OnMouseOver = function () {
+            if (isEditorMode)
+            {    
+                var x = $(this).data("x"),
+                    y = $(this).data("y"),
+                    diff = Math.ceil(penPix / 2),
+					total, i, j;
+
+                x -= diff;
+                y -= diff;
+                for (i = 0; i < penPix; i += 1) {
+                    for (j = 0; j < penPix; j += 1) {
+                        table.find("#cell_" + (x + i) + "_" + (y + j)).css("background-color", editorColor);
+                    }
+                }
 			}
-			$(this).append(fieldCell);
-		});
-		
-		table.find('.cellPix').bind('mouseover', OnMouseOver);
-		table.find('.cellPix').bind('click', OnClick);
-	};
+		};
+
+    this.Constructor = function (w, h) {
+        var tr = "<tr/>",
+            td = "<td/>",
+            cell = "<div class='cell'></div>",
+            cellPix = "<div class='cellPix'></div>",
+            i;
+        
+        for (i = 0; i < h; i += 1) {
+            table.append(tr);
+        }
+        for (i = 0; i < w; i += 1) {
+            table.find('tr').append(td);
+        }
+        
+        table.find('tr td').each(function (index) {
+            var fieldCell = $(cell),
+                i, j, c, x, y;
+
+            for (i = 0; i < 10; i += 1)
+            {
+                for (j = 0; j < 10; j += 1)
+                {
+                    c = $(cellPix);
+                    x = (index % width) * 10 + j;
+                    y = Math.floor(index / width) * 10 + i;
+
+                    c.data("x", x);
+                    c.data("y", y);
+                    c.attr('id', 'cell_' + x + '_' + y);
+                    c.css("background-color", "green");
+                    fieldCell.append(c);
+                }
+            }
+            $(this).append(fieldCell);
+        });
+        
+        table.find('.cellPix').bind('mouseover', OnMouseOver);
+        table.find('.cellPix').bind('click', OnClick);
+    };
+    
+    this.SetEditor = function (bool) {
+        isEditor = bool;
+    };
+    
+    this.GetEditor = function (bool) {
+        return isEditor;
+    };
+    
+    this.SetEditorColor = function (scolor) {
+        editorColor = scolor;
+    };
+    
+    this.GetField = function () {
+        return table;
+    };
+    
+    this.SetPen = function (pix) {
+        penPix = pix;
+    };
 	
-	this.SetEditor = function(bool)
-	{
-		isEditor = bool;
-	}
-	
-	this.GetEditor = function(bool)
-	{
-		return isEditor;
-	}
-	
-	this.SetEditorColor = function(scolor)
-	{
-		editorColor = scolor;
-	}
-	
-	var OnClick = function () {
-		if (isEditor)
-			isEditorMode = !isEditorMode;
-		
-		if (isEditorMode) {
-			table.find("td").css("border", "1px solid red");
-		}
-		else {
-			table.find("td").css("border", "1px solid black");
-		}
-	}
-	
-	var OnMouseOver = function () {
-		if (isEditorMode)
-		{	
-			var x = $(this).data("x");
-			var y = $(this).data("y");
-			var total;
-			var diff = Math.ceil(penPix / 2)
-			x -= diff;
-			y -= diff;
-			for(i=0; i<penPix; i++)
-			{
-				for(j=0; j<penPix; j++)
-				{
-					table.find("#cell_" + (x+i) + "_" + (y+j)).css("background-color", editorColor);
-				}
-			}
-		}
-	}
-	
-	this.GetField = function() {
-		return table;
-	}
-	
-	this.SetPen = function(pix) {
-		penPix = pix;
-	}
-	this.Constructor(width, height);
+    this.Constructor(width, height);
 }
--- js/token.js
+++ js/token.js
@@ -1,47 +1,53 @@
+/*jshint onevar: true, strict: true, curly: true, eqeqeq: true, laxbreak: true */
 ///////////////////////////////////////
 //// Token
 function Token(width, height)
 {
-	var token = $("<div class='cell'>&nbsp;</div>");
-	var info = $("<div class='info'></div>");
-	
-	this.Constructor = function (w, h) {
-		token.addClass('bggray');
-		token.draggable({snap : ".cell", snapMode : "inner"});
-		token.mouseover(function() {
-			if (!$(this).hasClass( "ui-draggable-dragging" )) {
-				var offset = $(this).offset();
-				token.append(info);
-				info.show();
-				info.offset({ top: offset.top, left: offset.left + 25});
-			}
-		});
-		
-		token.mouseout(function() {
-			info.hide();
-		});
-		
-		token.mousedown(function() {
-			info.hide();
-		});
-		
-		makeInfo();
-		
-	}
-	
-	var makeInfo = function() {
-		info.append("<input type='text' size='40' style='bold;'></input>");
-		info.append("<textarea rows='10' cols='30'></textarea>");
-		token.append(info);
-		info.hide();
-	}
-	
-	this.GetToken = function () {
-		return token;
-	}
-	
-	this.GetInfo = function () {
-		return info;
-	}
-	this.Constructor(width, height);
+    "use strict";
+    var token = $("<div class='cell'>&nbsp;</div>"),
+        info = $("<div class='info'></div>"),
+        
+        makeInfo = function () {
+            info.append("<input type='text' size='40' style='bold;'></input>");
+            info.append("<textarea rows='10' cols='30'></textarea>");
+            token.append(info);
+            info.hide();
+        };
+        
+    this.Constructor = function (w, h) {
+        token.addClass('bggray');
+        // Snap inner element with cell class
+        token.draggable({snap : ".cell", snapMode : "inner"});
+        
+        // Display information if token in not dragged
+        token.mouseover(function () {
+            if (!$(this).hasClass("ui-draggable-dragging")) {
+                var offset = $(this).offset();
+                token.append(info);
+                info.show();
+                info.offset({ top: offset.top, left: offset.left + 25});
+            }
+        });
+        
+        token.mouseout(function () {
+            info.hide();
+        });
+        
+        token.mousedown(function () {
+            info.hide();
+        });
+        
+        makeInfo();
+        
+    };
+    
+    this.GetToken = function () {
+        return token;
+    };
+    
+    this.GetInfo = function () {
+        return info;
+    };
+    
+    this.Constructor(width, height);
 }
